3542ce6d11d2e31236d5adf671ac8a33f694c867
Matheus Souza (Taak) <matheus.souza@taak.com.br>
2026-06-10 15:09:39 -0300

    feat: Stylization of FAQ screen

--- force-app/main/default/lwc/faq/faq.js
+++ force-app/main/default/lwc/faq/faq.js
@@ -6,6 +6,16 @@ export default class Faq extends LightningElement {
             id: 1,
             question: "Quem proclamou a independência?",
             answer: "<ul><li>Pedro Álvares Cabral</li></ul>"
-        }
+        },
+        {
+            id: 2,
+            question: "Quem proclamou a independência?",
+            answer: "Pedro Álvares Cabral"
+        },
+        {
+            id: 3,
+            question: "Quem proclamou a independência?",
+            answer: "Pedro Álvares Cabral"
+        },
     ]
 }
