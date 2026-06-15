bdf8e10df2751f08a31924a70898a09b4a814066
Matheus Souza (Taak) <matheus.souza@taak.com.br>
2026-06-15 12:17:01 -0300

    feat: Working FAQ search

--- force-app/main/default/lwc/faq/faq.js
+++ force-app/main/default/lwc/faq/faq.js
@@ -1,21 +1,63 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { gql, graphql } from "lightning/graphql";
 
 export default class Faq extends LightningElement {
-    faqs = [
-        {
-            id: 1,
-            question: "Quem proclamou a independência?",
-            answer: "<ul><li>Pedro Álvares Cabral</li></ul>"
-        },
-        {
-            id: 2,
-            question: "Quem proclamou a independência?",
-            answer: "Pedro Álvares Cabral"
-        },
-        {
-            id: 3,
-            question: "Quem proclamou a independência?",
-            answer: "Pedro Álvares Cabral"
-        },
-    ]
+    results;
+    errors;
+
+    searchString = '%';
+
+    @wire(graphql, {
+        query: gql`
+            query searchFaq($searchString: TextArea) {
+                uiapi {
+                    query {
+                        FAQ__c(
+                            where: { 
+                                Question__c: { like: $searchString }
+                            }        
+                        ) {
+                            edges {
+                                node {
+                                    Id
+                                    Question__c {
+                                        value
+                                    }
+                                    Answer__c {
+                                        value
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        `,
+        variables: '$variables'
+    })
+    getRecords({data, errors}) {
+        if (data) {
+            this.results = data.uiapi.query.FAQ__c.edges.map((edge) => edge.node);
+            console.log('Results:');
+            console.log(this.results);
+        }
+        this.errors = errors
+        console.log('Errors:');
+        console.log(this.errors);
+    }
+
+    get variables() {
+        return {
+            searchString: this.searchString,
+        };
+    }
+
+
+    handleSearch(event) {
+        setTimeout(() => {
+            this.searchString = '%' + event.detail.value.trim() + '%';
+            console.log(this.searchString);
+            console.log(this.variables);
+        }, 1000);
+    }
 }
