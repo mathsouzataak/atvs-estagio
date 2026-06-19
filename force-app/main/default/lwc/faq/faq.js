1698bc6c168e415a502678e1eb917deeb7b60393
Matheus Souza (Taak) <matheus.souza@taak.com.br>
2026-06-19 12:27:31 -0300

    feat: Started pagination development

--- force-app/main/default/lwc/faq/faq.js
+++ force-app/main/default/lwc/faq/faq.js
@@ -1,18 +1,30 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire,  } from 'lwc';
 import { gql, graphql } from "lightning/graphql";
 
 export default class Faq extends LightningElement {
     results;
-    errors;
+    errors; 
+
+    after = null;
+    pageNumber = 1;
+
+    afterBuffer;
+    cursorStack = [];
+    hasNextPage;
+    hasPreviousPage;
+    totalCount = 0;
+    
 
     searchString = '%';
 
     @wire(graphql, {
         query: gql`
-            query searchFaq($searchString: TextArea) {
+            query searchFaq($searchString: TextArea, $after: String) {
                 uiapi {
                     query {
                         FAQ__c(
+                            first: 10
+                            after: $after
                             where: { 
                                 Question__c: { like: $searchString }
                             }        
@@ -28,6 +40,13 @@ export default class Faq extends LightningElement {
                                     }
                                 }
                             }
+                            totalCount
+                            pageInfo {
+                                startCursor
+                                endCursor
+                                hasNextPage
+                                hasPreviousPage
+                            }
                         }
                     }
                 }
@@ -38,19 +57,41 @@ export default class Faq extends LightningElement {
     getRecords({data, errors}) {
         if (data) {
             this.results = data.uiapi.query.FAQ__c.edges.map((edge) => edge.node);
+            this.afterBuffer = data.uiapi.FAQ__c.pageInfo.endCursor;
+            this.hasNextPage = data.uiapi.FAQ__c.pageInfo.hasNextPage;
+            this.hasPreviousPage = data.uiapi.FAQ__c.pageInfo.hasPreviousPage;
+            this.totalCount = data.uiapi.FAQ__c.totalCount;
             console.log('Results:');
             console.log(this.results);
         }
         this.errors = errors
         console.log('Errors:');
-        console.log(this.errors);
+        console.log(this.errors[0].message);
     }
 
     get variables() {
         return {
             searchString: this.searchString,
+            after: this.after || null,
         };
     }
+
+    get currentPageNumber() {
+        return this.totalCount === 0 ? 0 : this.pageNumber;
+    }
+
+    get isFirstPage() {
+        return !this.hasPreviousPage;
+    }
+
+    get isLastPage() {
+        return !this.hasNextPage;
+    }
+
+    get totalPageCount() {
+        return Math.ceil(this.totalCount / 10);
+    }
+
 
 
     handleSearch(event) {
@@ -58,6 +99,24 @@ export default class Faq extends LightningElement {
             this.searchString = '%' + event.detail.value.trim() + '%';
             console.log(this.searchString);
             console.log(this.variables);
+            this.after = null;
+            this.cursorStack = [];
+            this.pageNumber = 1;
         }, 1000);
+    }
+
+    handleNext() {
+        if (this.hasNextPage) {
+            this.cursorStack.concat(this.after);
+            this.after = this.afterBuffer;
+            this.pageNumber++;
+        }
+    }
+
+    handlePrevious() {
+        if (this.hasPreviousPage) {
+            this.after = this.cursorStack.pop();
+            this.pageNumber--;
+        }
     }
 }
