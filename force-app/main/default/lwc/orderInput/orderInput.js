daa500104f80efd66f54fe6786658ec1bae3cf61
Matheus Souza (Taak) <matheus.souza@taak.com.br>
2026-07-15 11:41:09 -0300

    feat: replaced payment record picker for combobox with wire apex api

--- force-app/main/default/lwc/orderInput/orderInput.js
+++ force-app/main/default/lwc/orderInput/orderInput.js
@@ -1,14 +1,36 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getPaymentConditionList from '@salesforce/apex/OrderController.getPaymentConditionList';
 
 export default class OrderInput extends LightningElement {
+    
     accountId = "";
+    paymentConditionId;
+    addressId;
+    observations;
 
-
+    paymentConditionOptions = [];
+    
+    
+    
     handleAccountChange(event) {
         this.accountId = event.detail.recordId;
         console.log(this.accountId);
     }
 
+    handlePaymentSelected(event) {
+        this.paymentConditionId = event.target.value;
+    } 
+
+    handleAddressChange(event) {
+        this.addressId = event.detail.recordId;
+    }
+
+    handleObservations(event) {
+        this.observations = event.target.value
+    }
+
+
+    
     get addressFilter() {
         return {
             criteria: [
@@ -19,6 +41,22 @@ export default class OrderInput extends LightningElement {
                 }
             ]
         };
+    }
+
+
+    
+    @wire(getPaymentConditionList)
+    wiredPaymentConditions({error, data}) {
+        if (data) {
+            console.log('Combobox data:')
+            console.log(data);
+            this.paymentConditionOptions = data.map((record) => ({
+                value: record.Id,
+                label: record.Name
+            }));
+        } 
+    
+        console.log(error);
     }
 
 }
